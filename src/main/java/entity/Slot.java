a2b021229af734034e52a84ef37a9972c14421e9
Savva <savahits@gmail.com>
2026-06-05 18:06:07 +0300

    feat: add Slot entity with fields for room, schedule, start time, and end time

---
+++ src/main/java/entity/Slot.java
@@ -0,0 +1,38 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "slots")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Slot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id", nullable = false)
+    private Schedule schedule;
+
+    @Column(name = "start_at", nullable = false,
+            columnDefinition = "TIMESTAMPTZ")
+    private OffsetDateTime startAt;
+
+    @Column(name = "end_at", nullable = false,
+            columnDefinition = "TIMESTAMPTZ")
+    private OffsetDateTime endAt;
+}
