d1b237ed427ca3fa15c0a88b16dce54f313a98df
Savva <savahits@gmail.com>
2026-06-05 14:32:33 +0300

    initialize project

---
+++ src/main/java/ru/shmelev/roomsschedules/RoomsSchedulesApplication.java
@@ -0,0 +1,13 @@
+package ru.shmelev.roomsschedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsSchedulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomsSchedulesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/shmelev/roomsschedules/RoomsSchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shmelev.roomsschedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomsSchedulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
