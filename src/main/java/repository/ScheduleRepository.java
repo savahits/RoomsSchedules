a12b5945e067a4decfe74d876c1a7f590c254a01
Savva <savahits@gmail.com>
2026-06-05 18:09:55 +0300

    feat: add repository interfaces for Booking, Room, Schedule, Slot, and User entities

---
+++ src/main/java/repository/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+}
