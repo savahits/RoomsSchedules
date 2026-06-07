0d0ae18fa60cf1a2e39cc5ff66bd4d5828c026ef
Savva <savahits@gmail.com>
2026-06-08 01:55:50 +0300

    fix: allow access to swagger-ui.html in SecurityConfig

--- src/main/java/ru/shmelev/roomsschedules/config/SecurityConfig.java
+++ src/main/java/ru/shmelev/roomsschedules/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                         .requestMatchers("/register", "/login").permitAll()
                         .requestMatchers("/_info").permitAll()
 
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
 
                         // переговорки
                         .requestMatchers(HttpMethod.POST, "/rooms/**").hasRole("admin")
