f7576fff2bd11a49829bb84a754c3a0c13593d6b
Savva <savahits@gmail.com>
2026-06-08 01:15:26 +0300

    feat: add findMaxEndAtByScheduleId method to SlotRepository for retrieving maximum end time by schedule ID

--- src/main/java/ru/shmelev/roomsschedules/repository/SlotRepository.java
+++ src/main/java/ru/shmelev/roomsschedules/repository/SlotRepository.java
@@ -4,6 +4,11 @@ import ru.shmelev.roomsschedules.entity.Slot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface SlotRepository extends JpaRepository<Slot, Long> {
+    Optional<OffsetDateTime>  findMaxEndAtByScheduleId(UUID id);
 }
