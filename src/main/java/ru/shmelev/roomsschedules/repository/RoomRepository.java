73ba98099e639d2c2e0db8eb035fd990a4f49ba3
Savva <savahits@gmail.com>
2026-06-07 03:01:51 +0300

    feat: add findById method to RoomRepository for retrieving rooms by UUID

--- src/main/java/ru/shmelev/roomsschedules/repository/RoomRepository.java
+++ src/main/java/ru/shmelev/roomsschedules/repository/RoomRepository.java
@@ -3,7 +3,10 @@ package ru.shmelev.roomsschedules.repository;
 import ru.shmelev.roomsschedules.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface RoomRepository  extends JpaRepository<Room, Long> {
+    Optional<Room> findById(UUID id);
 }
