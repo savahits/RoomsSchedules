1f3a1051dedefbcd04731d81712e79d0e5ae27c8
Savva <savahits@gmail.com>
2026-06-07 23:35:18 +0300

    feat: enable scheduling in RoomsSchedulesApplication

--- src/main/java/ru/shmelev/roomsschedules/RoomsSchedulesApplication.java
+++ src/main/java/ru/shmelev/roomsschedules/RoomsSchedulesApplication.java
@@ -2,8 +2,10 @@ package ru.shmelev.roomsschedules;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RoomsSchedulesApplication {
 
     public static void main(String[] args) {
