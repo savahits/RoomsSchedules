8ad09276440a40d97b7903fed42e25f30312e883
Savva <savahits@gmail.com>
2026-06-07 03:01:39 +0300

    feat: add existsByRoomId method to ScheduleRepository for room ID existence check

--- src/main/java/ru/shmelev/roomsschedules/repository/ScheduleRepository.java
+++ src/main/java/ru/shmelev/roomsschedules/repository/ScheduleRepository.java
@@ -4,6 +4,9 @@ import ru.shmelev.roomsschedules.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    boolean existsByRoomId(UUID roomId);
 }
