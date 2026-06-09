0c55e23e0c3d024d6b13bf040e994eddf5def630
Savva <savahits@gmail.com>
2026-06-09 20:44:59 +0300

    feat: add Swagger tag for RoomController

--- src/main/java/ru/shmelev/roomsschedules/controller/RoomController.java
+++ src/main/java/ru/shmelev/roomsschedules/controller/RoomController.java
@@ -1,5 +1,6 @@
 package ru.shmelev.roomsschedules.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/rooms")
 @RequiredArgsConstructor
+@Tag(name = "Rooms")
 public class RoomController {
 
     private final RoomService roomService;
