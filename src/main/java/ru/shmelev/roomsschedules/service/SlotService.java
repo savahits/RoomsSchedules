22429fd1b5970aca119b8c6e5ae7b7524d9ef750
Savva <savahits@gmail.com>
2026-06-09 20:20:50 +0300

    feat: add room existence check in SlotService for available slots

--- src/main/java/ru/shmelev/roomsschedules/service/SlotService.java
+++ src/main/java/ru/shmelev/roomsschedules/service/SlotService.java
@@ -1,9 +1,11 @@
 package ru.shmelev.roomsschedules.service;
 
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.shmelev.roomsschedules.dto.response.SlotResponse;
 import ru.shmelev.roomsschedules.dto.response.SlotsResponse;
+import ru.shmelev.roomsschedules.repository.RoomRepository;
 import ru.shmelev.roomsschedules.repository.SlotRepository;
 
 import java.time.LocalDate;
@@ -16,13 +18,19 @@ import java.util.UUID;
 public class SlotService {
 
     private final SlotRepository slotRepository;
+    private final RoomRepository roomRepository;
 
-    public SlotService(final SlotRepository slotRepository) {
+    public SlotService(SlotRepository slotRepository, RoomRepository roomRepository) {
         this.slotRepository = slotRepository;
+        this.roomRepository = roomRepository;
     }
 
-    public SlotsResponse getAvailableSlots(UUID roomId, LocalDate date
-    ) {
+    public SlotsResponse getAvailableSlots(UUID roomId, LocalDate date) {
+
+        if (roomRepository.existsById(roomId) ==  false) {
+            throw new EntityNotFoundException("Переговорка не найдена");
+        }
+
         OffsetDateTime dayStart = date.atStartOfDay().atOffset(ZoneOffset.UTC);
 
         OffsetDateTime dayEnd = dayStart.plusDays(1);
