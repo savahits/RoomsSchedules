5184208330934d75dbe25c232092cbf2c44faa4e
Savva <savahits@gmail.com>
2026-06-11 18:25:48 +0300

    refactor: remove duplicate import statement in RoomRepository

--- src/main/java/ru/shmelev/roomsschedules/repository/RoomRepository.java
+++ src/main/java/ru/shmelev/roomsschedules/repository/RoomRepository.java
@@ -1,9 +1,8 @@
 package ru.shmelev.roomsschedules.repository;
 
+import ru.shmelev.roomsschedules.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.shmelev.roomsschedules.entity.Room;
-
 import java.util.Optional;
 import java.util.UUID;
 
