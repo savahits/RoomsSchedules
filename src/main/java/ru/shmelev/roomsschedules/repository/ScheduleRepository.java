bf3037d1c0f14e5b076c7401e4c0d1d3deb0e326
Savva <savahits@gmail.com>
2026-06-11 18:25:55 +0300

    refactor: remove duplicate import statement in ScheduleRepository

--- src/main/java/ru/shmelev/roomsschedules/repository/ScheduleRepository.java
+++ src/main/java/ru/shmelev/roomsschedules/repository/ScheduleRepository.java
@@ -1,8 +1,8 @@
 package ru.shmelev.roomsschedules.repository;
 
+import ru.shmelev.roomsschedules.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.shmelev.roomsschedules.entity.Schedule;
 
 import java.util.UUID;
 
