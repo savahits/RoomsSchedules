0edb05b21364c4f4fcb05ef105fa5d4f5a802d0c
Savva <savahits@gmail.com>
2026-06-11 18:25:38 +0300

    refactor: add missing import statement for Map in JwtService

--- src/main/java/ru/shmelev/roomsschedules/security/JwtService.java
+++ src/main/java/ru/shmelev/roomsschedules/security/JwtService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
