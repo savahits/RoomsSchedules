43b873c09f5b016d0e480d400a99c40f3299013e
Savva <savahits@gmail.com>
2026-06-11 18:26:12 +0300

    refactor: remove duplicate import statement for User in UserRepository

--- src/main/java/ru/shmelev/roomsschedules/repository/UserRepository.java
+++ src/main/java/ru/shmelev/roomsschedules/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package ru.shmelev.roomsschedules.repository;
 
+import ru.shmelev.roomsschedules.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.shmelev.roomsschedules.entity.User;
 
 import java.util.UUID;
 
