40846688a609275e9e68622d05626c3059b1b02c
Savva <savahits@gmail.com>
2026-06-11 18:25:14 +0300

    refactor: streamline import statements in AuthController

--- src/main/java/ru/shmelev/roomsschedules/controller/AuthController.java
+++ src/main/java/ru/shmelev/roomsschedules/controller/AuthController.java
@@ -3,10 +3,7 @@ package ru.shmelev.roomsschedules.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.shmelev.roomsschedules.dto.response.TokenResponse;
 import ru.shmelev.roomsschedules.security.JwtService;
 
