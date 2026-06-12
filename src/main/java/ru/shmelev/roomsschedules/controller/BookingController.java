ec0d4cdf4720ce2cef1b0a45883211114b46ab81
Savva <savahits@gmail.com>
2026-06-12 21:36:32 +0300

    feat: add endpoint to list all bookings in BookingController

--- src/main/java/ru/shmelev/roomsschedules/controller/BookingController.java
+++ src/main/java/ru/shmelev/roomsschedules/controller/BookingController.java
@@ -1,18 +1,17 @@
 package ru.shmelev.roomsschedules.controller;
 
 import org.apache.coyote.BadRequestException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.shmelev.roomsschedules.dto.response.BookingResponse;
 import ru.shmelev.roomsschedules.service.BookingService;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/bookings")
+@RequestMapping("/bookings")
 public class BookingController {
 
     private final BookingService bookingService;
@@ -21,11 +20,17 @@ public class BookingController {
         this.bookingService = bookingService;
     }
 
-    @PostMapping("/{slotId}")
+    @PostMapping("/create/{slotId}")
     public BookingResponse createBooking(@PathVariable UUID slotId, Authentication authentication) throws BadRequestException {
 
         UUID userId = (UUID) authentication.getPrincipal();
 
         return bookingService.createBooking(slotId, userId);
+    }
+
+    @GetMapping("/list")
+    @PreAuthorize("hasRole('admin')")
+    public List<BookingResponse> listBookings() throws BadRequestException {
+        return bookingService.getAllBookings();
     }
 }
