6aa0014f0d3de6b24436449a50930ba34cdb6094
Savva <savahits@gmail.com>
2026-06-14 14:42:13 +0300

    feat: add findAllByUser_IdAndSlot_StartAtGreaterThanEqual method for BookingRepository

--- src/main/java/ru/shmelev/roomsschedules/repository/BookingRepository.java
+++ src/main/java/ru/shmelev/roomsschedules/repository/BookingRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.shmelev.roomsschedules.entity.Booking;
 
+import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
     boolean existsActiveBookingBySlotId(UUID slotId);
+
+    List<Booking> findAllByUser_IdAndSlot_StartAtGreaterThanEqual(UUID userId, OffsetDateTime startAt);
+
 }
